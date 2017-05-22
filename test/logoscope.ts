9eae4f2026f1fdae2d25b9e0c8bb276ad8b6f0c2
Jayesh Salvi <jayeshsalvi@gmail.com>
2017-05-22 19:55:43 -0400

    plot bspline curve in logoscope

--- test/logoscope.ts
+++ test/logoscope.ts
@@ -20,10 +20,32 @@
 
 */
 
-import blueplot from 'blueplot'
+import * as blueplot from 'blueplot'
+import {Vector2} from '../src/basic'
+import {BSplineCurve2D} from '../src/geom/nurbs'
 
 window.onload = () => {
 
-  console.log(blueplot);
+  let plot = new blueplot.Plot(600,400);
+  document.body.appendChild(plot.dom);
+
+  let cpoints = [
+    new Vector2(10,10),
+    new Vector2(40,130),
+    new Vector2(150,200),
+    new Vector2(200,200)
+  ];
+
+  let bcrv1 = new BSplineCurve2D(3,cpoints,[0,0,0,0,1,1,1,1]);
+  let points = [];
+  for(let i=0; i<101; i++) {
+    points.push(bcrv1.evaluate(i/100).toArray());
+  }
+  let dg = new blueplot.Datagroup2D(plot.width,plot.height);
+  dg.addPoints(points,{type:'line'});
+  dg.addPoints(cpoints.map(v => v.toArray()),{type:'scatter'});
+  plot.add(dg);
+
+  plot.render();
 
 }
