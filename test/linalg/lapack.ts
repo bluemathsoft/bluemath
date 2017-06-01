b720ffc338874436cc2cce3cc93d25a8b485344b
Jayesh Salvi <jayeshsalvi@gmail.com>
2017-06-01 18:10:20 -0400

    minor reorg of blas l1 tests

--- test/linalg/lapack.ts
+++ test/linalg/lapack.ts
@@ -8,20 +8,28 @@ export default function testLAPACK() {
 
     QUnit.module('BLAS Level 1', () => {
 
-      QUnit.test('asum', assert => {
-        let sx = new NDArray([1,2,3,4]);
-        assert.equal(linalg.lapack.asum(sx.data), 10);
-        let dx = new NDArray([1,2,3,4],{datatype:'f64'});
-        assert.equal(linalg.lapack.asum(dx.data), 10);
+      QUnit.module('asum', () => {
+        QUnit.test('sasum', assert => {
+          let sx = new NDArray([1,2,3,4]);
+          assert.equal(linalg.lapack.asum(sx.data), 10);
+        });
+        QUnit.test('dasum', assert => {
+          let dx = new NDArray([1,2,3,4],{datatype:'f64'});
+          assert.equal(linalg.lapack.asum(dx.data), 10);
+        });
       });
-      QUnit.test('dot', assert => {
-        let sx = new NDArray([1,2,3,4]);
-        let sy = new NDArray([2,3,4,5]);
-        assert.equal(linalg.lapack.dot(sx.data,sy.data), 40);
 
-        let dx = new NDArray([1,2,3,4],{datatype:'f64'});
-        let dy = new NDArray([2,3,4,5],{datatype:'f64'});
-        assert.equal(linalg.lapack.dot(dx.data,dy.data), 40);
+      QUnit.module('dot', () => {
+        QUnit.test('sdot', assert => {
+          let sx = new NDArray([1,2,3,4]);
+          let sy = new NDArray([2,3,4,5]);
+          assert.equal(linalg.lapack.dot(sx.data,sy.data), 40);
+        });
+        QUnit.test('ddot', assert => {
+          let dx = new NDArray([1,2,3,4],{datatype:'f64'});
+          let dy = new NDArray([2,3,4,5],{datatype:'f64'});
+          assert.equal(linalg.lapack.dot(dx.data,dy.data), 40);
+        });
       });
 
     });
