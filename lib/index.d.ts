bf5f16ca844d16f3b7a68ccccb96d5d99f1bba12
Jayesh Salvi <jayeshsalvi@gmail.com>
2017-07-06 10:22:49 -0400

    0.2.11 publish

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -4,5 +4,5 @@ import { EPSILON } from './constants';
 import { NDArray, Matrix, Vector, Vector2, Vector3, Complex, PermutationVector, BandMatrix } from './basic';
 export declare type NumberType = 'i8' | 'ui8' | 'i16' | 'ui16' | 'i32' | 'ui32' | 'f32' | 'f64';
 export declare type TypedArray = Int8Array | Uint8Array | Int16Array | Uint16Array | Int32Array | Uint32Array | Float32Array | Float64Array;
-export declare const version: any;
+export declare const version = "0.2.11";
 export { linalg, EPSILON, NDArray, Matrix, Vector, Vector2, Vector3, Complex, PermutationVector, BandMatrix, eye, zeros, iszero, isequal, torad, todeg, add, mul, sub, div };
--- src/index.ts
+++ src/index.ts
@@ -38,7 +38,7 @@ export type NumberType = 'i8'|'ui8'|'i16'|'ui16'|'i32'|'ui32'|'f32'|'f64';
 export type TypedArray = Int8Array | Uint8Array | Int16Array |
     Uint16Array | Int32Array | Uint32Array | Float32Array | Float64Array;
 
-export const version = require('../package.json').version;
+export const version = '0.2.11'; // TODO: populate from package.json
 
 export {
   // Submodules
