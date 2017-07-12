0d3796f6c03c601e6853f03723e41156d2aa11a0
Jayesh Salvi <jayeshsalvi@gmail.com>
2017-07-12 11:29:21 -0400

    Add tests to ndarray C implementation (using CuTest)

---
+++ native/ndarray/AllTests.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#include "CuTest.h"
+
+CuSuite* NDArrayTestSuite();
+
+void RunAllTests(void)
+{
+	CuString *output = CuStringNew();
+	CuSuite* suite = CuSuiteNew();
+
+	CuSuiteAddSuite(suite, NDArrayTestSuite());
+
+	CuSuiteRun(suite);
+	CuSuiteSummary(suite, output);
+	CuSuiteDetails(suite, output);
+	printf("%s\n", output->buffer);
+}
+
+int main(void)
+{
+	RunAllTests();
+}
---
+++ native/ndarray/NDArrayTests.c
@@ -0,0 +1,18 @@
+
+#include <string.h>
+#include "CuTest.h"
+
+#include "ndarray.h"
+
+void TestNDArrayConstruction(CuTest *tc) {
+  uint shape[] = {2,2};
+  NDArray *ndarr = nda_create(f32,2,shape,NULL);
+  CuAssertPtrNotNull(tc,ndarr);
+  nda_destroy(ndarr);
+}
+
+CuSuite* NDArrayTestSuite() {
+  CuSuite* suite = CuSuiteNew();
+  SUITE_ADD_TEST(suite, TestNDArrayConstruction);
+  return suite;
+}
