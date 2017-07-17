8dd41c0a191d8dcf1c9d775f1603c70c966d38d5
Jayesh Salvi <jayeshsalvi@gmail.com>
2017-07-17 16:04:50 -0400

    fix that makes getter-setter working

--- native/bmnative-cli.js
+++ native/bmnative-cli.js
@@ -3,4 +3,6 @@ const Module = require('./build-em/bmnative');
 
 let ndarr = new Module.NDArrayInt32([5,6,7],3);
 ndarr.set([0,0,0],3,276);
+ndarr.set([0,1,2],3,-45);
 console.log(ndarr.get([0,0,0],3));
+console.log(ndarr.get([0,1,2],3));
