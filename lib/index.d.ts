482052d72669bcd1a63ae78f21c8a445468bd3b7
Jayesh Salvi <jayeshsalvi@gmail.com>
2017-08-07 13:27:06 -0400

    publish 0.3.2 with submodule based architecture

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -3,5 +3,5 @@ import * as linalg from '@bluemath/linalg';
 import * as geom from '@bluemath/geom';
 export declare type NumberType = 'i8' | 'ui8' | 'i16' | 'ui16' | 'i32' | 'ui32' | 'f32' | 'f64';
 export declare type TypedArray = Int8Array | Uint8Array | Int16Array | Uint16Array | Int32Array | Uint32Array | Float32Array | Float64Array;
-export declare const version = "0.2.13";
+export declare const version = "0.3.2";
 export { linalg, geom, EPSILON, NDArray, Complex, eye, zeros, empty, range, iszero, isequal, torad, todeg, add, mul, sub, div, count };
