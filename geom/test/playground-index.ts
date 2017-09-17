523642658e958fa462ae87a0663ce1724ed5da40
Jayesh Salvi <jayeshsalvi@gmail.com>
2017-09-17 09:31:44 -0400

    experiments with plotly layout
    resizing and landscape/portrait mode dynamically suites window size

--- geom/test/playground-index.ts
+++ geom/test/playground-index.ts
@@ -23,20 +23,73 @@ import $ = require("jquery");
 require('select2');
 import * as Plotly from 'plotly.js/lib/core'
 
-$(document).ready(function () {
-  $('.js-example-basic-single').select2({
-  }).on('change',function() {
-    console.log($('.js-example-basic-single').val());
-  });
+function doPlot(plotDiv,width=600,height=600) {
+  let layout:Partial<Plotly.Layout> = {
+    showlegend : false
+  };
+  let RANGE = [0,25];
+  let margin = 0.0;
 
+  if(width < height) { // Portrait - Vertical stack
+    layout.yaxis = { range:RANGE, domain:[0,0.5-margin] };
+    layout.yaxis2 = { range:RANGE, domain:[0.5+margin,1] };
+  } else { // Landscape - Horizontal stack
+    layout.xaxis = { range:RANGE, domain:[0,0.5-margin] };
+    layout.xaxis2 = { range:RANGE, domain:[0.5+margin,1] };
+    layout.yaxis2 = { range:RANGE, anchor : 'x2' };
+  }
+  layout.margin = {
+    t:0,
+    b:0,
+    l:0,
+    r:0
+  }
+  plotDiv.style.width = width+'px';
+  plotDiv.style.height = height+'px';
+
+	Plotly.newPlot(plotDiv, [
+      {
+        x: [1, 2, 3, 4, 5],
+        y: [1, 2, 4, 3, 16],
+        xaxis : 'x1',
+        yaxis : 'y1'
+      },
+      {
+        x: [1, 2, 3, 4, 5],
+        y: [1, 20, 4, 8, 16],
+        xaxis : 'x2',
+        yaxis : 'y2'
+      }
+    ], layout
+    /*
+    {
+
+      // Vertical
+      xaxis : {range:[0,25]},
+      yaxis : { range:[0,25], domain : [0,0.5]},
+      yaxis2 : { range:[0,25], domain : [0.5,1]}
+
+      // Horizontal
+      //xaxis : { domain:[0,0.45]},
+      //xaxis2 : { domain:[0.55,1]},
+      // yaxis2 : { anchor:'x2' }
+    }
+    */
+  );
+
+}
+
+$(document).ready(function () {
+  // $('.js-example-basic-single').select2({
+  // }).on('change',function() {
+  //   console.log($('.js-example-basic-single').val());
+  // });
   let plotDiv = document.createElement('div');
   document.body.appendChild(plotDiv);
-  plotDiv.style.width = '600px';
-  plotDiv.style.height = '600px';
 
-	Plotly.plot( plotDiv, [{
-    x: [1, 2, 3, 4, 5],
-    y: [1, 2, 4, 8, 16] }], {
-    margin: { t: 0 } }
-  );
+  doPlot(plotDiv,window.innerWidth-50, window.innerHeight-50);
+
+  $(window).resize(function () {
+    doPlot(plotDiv,window.innerWidth-50, window.innerHeight-50);
+  });
 });
