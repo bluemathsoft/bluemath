c02e56b06065af53c38315e3aba95110621891fe
Jayesh Salvi <jayeshsalvi@gmail.com>
2017-10-18 13:35:02 +0530

    0.4.2 publish

--- src/index.ts
+++ src/index.ts
@@ -21,11 +21,12 @@
 */
 
 import {
-  eye, zeros, empty,
+  eye, zeros, empty, arr,
   range,
   iszero, isequal,
   torad, todeg,
   add, mul, sub, div,
+  dot, cross, length, dir,
   count,
 
   NDArray, Complex,
@@ -41,7 +42,7 @@ export type NumberType = 'i8'|'ui8'|'i16'|'ui16'|'i32'|'ui32'|'f32'|'f64';
 export type TypedArray = Int8Array | Uint8Array | Int16Array |
     Uint16Array | Int32Array | Uint32Array | Float32Array | Float64Array;
 
-export const version = '0.3.3'; // TODO: populate from package.json
+export const version = '0.4.2'; // TODO: populate from package.json
 
 export {
   // Submodules
@@ -56,11 +57,12 @@ export {
   NDArray, Complex,
 
   // Ops
-  eye, zeros, empty,
+  eye, zeros, empty, arr,
   range,
   iszero, isequal,
   torad, todeg,
   add, mul, sub, div,
+  dot, cross, length, dir,
   count
 }
 
