21f3db8e174cdd3c6177b3e61251176085489823
Jayesh Salvi <jayeshsalvi@gmail.com>
2017-10-19 18:32:10 +0530

    0.4.3 publish

--- src/index.ts
+++ src/index.ts
@@ -29,7 +29,7 @@ export type NumberType = 'i8'|'ui8'|'i16'|'ui16'|'i32'|'ui32'|'f32'|'f64';
 export type TypedArray = Int8Array | Uint8Array | Int16Array |
     Uint16Array | Int32Array | Uint32Array | Float32Array | Float64Array;
 
-export const version = '0.4.2'; // TODO: populate from package.json
+export const version = '0.4.3'; // TODO: populate from package.json
 
 export {
   // Submodules
